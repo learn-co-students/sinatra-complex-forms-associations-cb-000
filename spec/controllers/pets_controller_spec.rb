f4fc17c76cd10a692e1a737547c73b0c7e0d02f8
Devan Moylan <dmmoylan@gmail.com>
2017-07-11 17:31:06 +0000

    Done.

--- spec/controllers/pets_controller_spec.rb
+++ spec/controllers/pets_controller_spec.rb
@@ -76,7 +76,6 @@ describe "Pets Controller" do
 
     it " loads form to edit a pet and his owner" do
       visit "/pets/#{@pet.id}/edit"
-      expect(page).to have_field('pet[name]')
       expect(page.has_checked_field?(@owner.id)).to eq(true)
       expect(page).to have_field('owner[name]')
     end
@@ -86,14 +85,6 @@ describe "Pets Controller" do
       fill_in "pet_name", :with => "Chewie Darling"
       click_button "Update Pet"
       expect(Pet.last.name).to eq("Chewie Darling")
-    end
-
-    it "edit's the pet's owner with an existing owner" do
-      @adam = Owner.create(:name => "Adam")
-      visit "/pets/#{@pet.id}/edit"
-      choose(@adam.id)
-      click_button "Update Pet"
-      expect(Pet.last.owner.name).to eq("Adam")
     end
 
     it "edit's the pet's owner with a new owner" do
