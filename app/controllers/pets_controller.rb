f1cbb823afd5cd25be2348a89c536be92edd4b2e
Alex Segers <a.segers.dev@gmail.com>
2018-05-21 21:02:34 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -25,9 +25,10 @@ class PetsController < ApplicationController
   post '/pets/:id' do
     @pet = Pet.find(params[:id])
     @pet = Pet.update(name: params["pet"]["name"])
-    @pet.owner = Owner.find(params["owner_id"]) if !params["owner_id"].empty?
-    @pet.owner = Owner.create(name: params["owner_name"]) if !params["owner_name"].empty?
-    @pet.save
-    redirect to "pets/#{@pet.id}"
+    @owner = Owner.find(params["owner_id"]) if !params["owner_id"].empty?
+    @owner = Owner.create(name: params["owner_name"]) if !params["owner_name"].empty?
+    @owner.pets << @pet
+    @owner.save
+    redirect to "/pets/#{@pet.id}"
   end
 end
