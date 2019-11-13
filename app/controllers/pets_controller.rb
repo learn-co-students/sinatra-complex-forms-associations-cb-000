d7f8c7b7a98d7b3eb4c6f1c92a95f67972d0a0ab
s-blais <stephen@s-blais.com>
2019-11-13 16:15:17 -0500

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -5,13 +5,27 @@ class PetsController < ApplicationController
     erb :'/pets/index' 
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
+    @pet = Pet.create(:name => params[:pet_name])
+    if params[:owner_id]
+      @pet.owner_id = params[:owner_id]
+    else
+      @owner = Owner.create(:name => params[:owner_name])
+      @pet.owner_id = @owner.id
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
+  end
+
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    erb :'/pets/edit'
   end
 
   get '/pets/:id' do 
@@ -20,7 +34,16 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do 
-
+    @pet = Pet.find(params[:id])
+    if params[:owner_name] == ""
+      @pet.update(:name => params[:pet_name], :owner_id => params[:owner_id])
+    else
+      @owner = Owner.create(:name => params[:owner_name])
+      @pet.update(:name => params[:name], :owner_id => @owner.id)
+    end
     redirect to "pets/#{@pet.id}"
   end
+
 end
+
+# ok now I see that I flattened the parameters being returned by the forms rather than than doing params[:pet] and params[:owner]... I guess that makes sense
